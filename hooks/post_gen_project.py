a38cd2669a742b2021da590987b60d98d4bcc86f
Alec Guintu <aeguintu@gmail.com>
2024-10-14 18:44:22 +0800

    Add post hook to execute post hook stuff

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+project_name = '{{ cookiecutter.project_name }}'
+print(f"Current working directory: {os.getcwd()}")
+
+subprocess.run(['go', 'mod', 'init', project_name], check=True)
+subprocess.run(['go', 'mod', 'tidy'], check=True)
+
+print(f"\nSAM initialized for service {project_name}.\n")
+
+print("Next steps:")
+print(f"1. Run `cd {project_name}` to switch to your new project directory.")
+print("2. Fill out the `.secrets.local.json` file with your environment variables.")
+print("3. Run `make dev` to start the local development environment.")
+print("  3.1. Optionally, use `make dev-watch` for live reloading.\n")
